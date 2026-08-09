2092c34f80a8eaa2fee86594cb51c47cac06f049
Dzegheim <61243095+Dzegheim@users.noreply.github.com>
2026-08-09 16:30:18 +0200

    Add explanatory comments to trivial test.

--- tests/is_it_alive_test.cpp
+++ tests/is_it_alive_test.cpp
@@ -1,5 +1,12 @@
+
+
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Catch2 runs. If this fails there's nowhere left to hide.", "[is_it_alive]") {
+    // GIVEN a build with Catch2 and ctest
+    // WHEN  the test suite is run
+    // THEN  this runs proving that the test framework exists and functions
+    
+    // The assertion is deliberately trivial and can never fail for any reason connected to this project. If it fails, the testing suite is broken, not the project.
     REQUIRE(2 + 2 == 4);
 }
