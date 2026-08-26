1a97bf30569bcee6add6c58a01187e77b9eba4e0
Dzegheim <61243095+Dzegheim@users.noreply.github.com>
2026-08-26 22:35:09 +0200

    Add doxygen documentation for constants.hpp.

--- include/hmeigens/constants.hpp
+++ include/hmeigens/constants.hpp
@@ -1,3 +1,5 @@
+/// @file
+/// @brief Constants used throughout the project.
 #ifndef HMEIGENS_CONSTANTS_HPP
 #define HMEIGENS_CONSTANTS_HPP
 
@@ -6,17 +8,19 @@
 #include <limits>
 
 namespace hmeigens {
-
-    // The difference between 1.0 and the next Scalar. 
-    // Values: 1.19209e-07 float, 2.22045e-16 double.
-    // Source: https://en.cppreference.com/cpp/types/climits.
+    /// @brief The difference between `1.0` and the next `Scalar`.
+    ///
+    /// [Values](https://en.cppreference.com/cpp/types/climits):
+    /// - `1.19209e-07` for `float`;
+    /// - `2.22045e-16` for  `double`.
     inline constexpr hmeigens::Scalar epsilon = std::numeric_limits<Scalar>::epsilon();
 
-    // Tolerance for parsing.
-    // The parsed value must be within 1 ULP of the input.
-    // No arithmetic is performed on the value, so tolerance is the tightest each type allows.
-    // SOURCE: https://en.cppreference.com/cpp/utility/from_chars
-    // SOURCE: "In any case, the resulting value is one of at most two floating-point values closest to the value of the string matching the pattern, after rounding according to std::round_to_nearest."
+    /// @brief Tolerance for parsing tests.
+    ///
+    /// The parsed value must be within 1 ULP of the input.
+    /// No arithmetic is performed on the value, so tolerance is the tightest each type allows.
+    /// [Source](https://en.cppreference.com/cpp/utility/from_chars):
+    /// > In any case, the resulting value is one of at most two floating-point values closest to the value of the string matching the pattern, after rounding according to `std::round_to_nearest`.
     inline constexpr hmeigens::Scalar parseTolerance = epsilon;
 }
 
