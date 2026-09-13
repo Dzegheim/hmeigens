b711136c2c9cbc90b2438c03191f95fe75d37344
Dzegheim <61243095+Dzegheim@users.noreply.github.com>
2026-09-13 11:44:32 +0200

    Fix wording in testing message for consistency with other tests.

--- tests/is_it_alive_test.cpp
+++ tests/is_it_alive_test.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Catch2 runs. If this fails there's nowhere left to hide.", "[is_it_alive]") {
+TEST_CASE("Catch2 runs test: if this fails there's nowhere left to hide.", "[is_it_alive]") {
     // GIVEN a build with Catch2 and ctest
     // WHEN  the test suite is run
     // THEN  this runs proving that the test framework exists and functions
